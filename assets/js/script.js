7d9b63ac3ae3de4bbabb3c5e618ec794aac99ea2
dev-rashidul <rashidul.contact@gmail.com>
2023-05-25 12:41:13 +0600

    Tutor Steps Section Done

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,12 @@ const img = document.querySelector("img")
 countryDropdown.addEventListener("click", () =>{
     countryList.classList.toggle("active")
 })
+
+
+// JavaScript For Sticky Header
+
+window.addEventListener("scroll", function(){
+    const header = document.querySelector(".navbar");
+    header.classList.toggle("sticky", window.scrollY > 100)
+
+})
