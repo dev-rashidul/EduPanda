098e76ded19d7b4e997c7f8f3c8c9c245d660079
dev-rashidul <rashidul.contact@gmail.com>
2023-05-29 23:20:43 +0600

    Register Login Page done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,43 @@
+// Register Section JavaScript
+
+const body_overlay = document.getElementById("body-overlay")
+const register_sidebar = document.getElementById("register_sidebar")
+const register_btn = document.getElementById("register_btn")
+const cross_btn = document.getElementById("cross_btn")
+
+register_btn.addEventListener("click", ()=>{
+    body_overlay.classList.toggle("active")
+    register_sidebar.classList.toggle("active")
+})
+
+cross_btn.addEventListener("click", ()=>{
+    body_overlay.classList.remove("active")
+    register_sidebar.classList.remove("active")
+})
+
+// Login Section JavaScript
+
+const login_sidebar = document.getElementById("login_sidebar")
+const login_btn = document.getElementById("login_btn")
+const cross_btn2 = document.getElementById("cross_btn2")
+
+login_btn.addEventListener("click", ()=>{
+    body_overlay.classList.toggle("active")
+    login_sidebar.classList.toggle("active")
+})
+
+
+cross_btn2.addEventListener("click", ()=>{
+    body_overlay.classList.remove("active")
+    login_sidebar.classList.remove("active")
+})
+
+
 // Language Switcher
 
 const countryDropdown = document.querySelector(".country-dropdown")
 const countryList = document.querySelector(".country-list")
-const selectedCountry = document.querySelector(".selected")
-const img = document.querySelector("img")
+
 
 countryDropdown.addEventListener("click", () =>{
     countryList.classList.toggle("active")
@@ -17,3 +51,4 @@ window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", window.scrollY > 100)
 
 })
+
